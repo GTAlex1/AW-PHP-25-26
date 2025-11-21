30530923b29238bc5c3850dce3616318e1b5b976
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-11-21 13:31:08 +0100

    o

---
+++ a3.php
@@ -0,0 +1,8 @@
+Actividad 3: Variables personales
+Declara tres variables con tu nombre, edad y ciudad, y muestra una frase combinando texto y variables.
+<?php
+$nombre = "Alejandro Fernández López-Cepero";
+$edad = 17;
+$ciudad = "Jerez de la Frontera, Cadiz";
+echo "Hola, mi nombre es $nombre, tengo $edad años y vivo en $ciudad.";
+?>
