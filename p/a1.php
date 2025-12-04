a15254478eb8e474ffcd48e42fd2b346a276c6bc
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-04 13:36:08 +0100

    a

--- p/a1.php
+++ p/a1.php
@@ -1,18 +1,4 @@
 <?php
-    /* Explicación de esto:
-    <?php
-        declare(strict_types=1);
-
-        function monstrarMensaje(string $mensaje): void {
-            echo $mensaje;
-        }
-
-        monstrarMensaje("Hola a todos");
-        echo PHP_EOL;
-    ?> */ 
-?>
-
-<?php
     declare(strict_types=1);
     function saludo(string $saludo): string {
         return "Hola, $saludo, bienvenido a mi primer script de PHP";
