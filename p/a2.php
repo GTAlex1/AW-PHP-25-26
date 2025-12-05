f5fbeb9c54982710bd4d7c706dcef13bc6493241
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-05 13:02:24 +0100

    a

--- p/a2.php
+++ p/a2.php
@@ -1,6 +1,6 @@
 <?php
     declare(strict_types=1);
-    const LENGUAGE = "PHP 8.2"
+    const LENGUAGE = "PHP 8.2";
     function presentacion(string $nombre): string;
         return "Hola, $saludo, bienvenido a mi primer script de $LENGUAGE";
 
