f568845625fb2c0c9e8f8ecce1a37ac3e5bb3f94
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-05 14:11:24 +0100

    a

--- p/a4.php
+++ p/a4.php
@@ -3,17 +3,17 @@
 
     $miPerfil = [
         "nombre" => "Kazus",
-        "edad" => "17",
+        "edad" => 17,
         "juego" => ["Honkai: Star Rail", "Colorful Stage"],
-        "nivel" => ["70", "18"]
+        "nivel" => [70, 18]
     ];
 
     echo sprintf(
         "Hola, me llamo %s, tengo %d, y soy nivel %d en %s",
-        $miPerfil[nombre],
-        $miPerfil[edad],
-        $miPerfil[nivel][1],
-        $miPerfil[juego][1]
+        $miPerfil['nombre'],
+        $miPerfil['edad'],
+        $miPerfil['nivel'][1],
+        $miPerfil['juego'][1]
     );
     echo PHP_EOL;
 ?>
