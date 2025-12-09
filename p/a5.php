7ce10f45e20f3dfaf0f7e940c027a24affd9eced
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-09 10:52:05 +0100

    foreach

---
+++ p/a5.php
@@ -0,0 +1,18 @@
+<?php
+/* Declaramos el strict type */
+declare(strict_types=1);
+
+/* Añadimos el array proporcionado en el enunciado */
+$miPerfil = [
+    "nombre" => "Kazus",
+    "edad"   => 17,
+    "juego"  => "Honkai: Star Rail",
+    "nivel"  => 70
+];
+
+/* Foreach */
+foreach ($miPerfil as $clave => $valor) {
+    echo "$clave: $valor" . PHP_EOL;
+}
+
+?>
