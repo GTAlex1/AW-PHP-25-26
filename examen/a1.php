853deea4683ac216c6f58b63c2cf76921bd1c48f
Kazus <120459752+GTAlex1@users.noreply.github.com>
2026-01-13 10:42:32 +0100

    a

---
+++ examen/a1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    
+<?php
+// Crea una función llamada mostrarPresentación que reciba el nombre, curso y módulo, el número de veces que debe mostrarse por pantalla. Se debe mostrar en una tabla (usar for).
+function mostrarPresentación($nombre, $curso, $modulo) {
+    for ($i=1; $i <= $repeticiones  ; $i++) { 
+        echo 
+    }
+}
+?>
+
+<table>
+    <?php mostrarPresentación(Alejandro, 2SMR, APWEB)?>
+</table>
+</body>
+</html>
