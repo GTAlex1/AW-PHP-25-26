ed39d9413465c1e45f7a247bf1e139e5b10d9e47
Kazus <120459752+GTAlex1@users.noreply.github.com>
2026-01-14 21:14:26 +0100

    A

--- examen/a2.php
+++ examen/a2.php
@@ -1,12 +1,15 @@
 <?php
 /* Crea una función llamada clasificarNotas que reciba 3 calificaciones numéricas y devuelva la media de estos valores si todos son mayores que 5, un 4 si alguna nota es menor a 5, y 
 un 2 si todas las notas es mejor que 5. Una vez calculado poner la nota en textual (Aprobado: para mayor que 5, Suspenso para 4 e Deficiente para 2).*/
-$aprobado = "";
+$nota1=9;
+$nota2=9;
+$nota3=9;
+
 function clasificarNotas($nota1, $nota2, $nota3) {
     if ($nota1 > 5 && $nota2 > 5 && $nota3 > 5 ) {
         $estado = "Aprobado";
         $media = ($nota1 + $nota2 + $nota3)/3;
-    } elseif ($nota1 < 5 || $nota2 < 5 || $nota3 > 5) {
+    } elseif ($nota1 < 5 || $nota2 < 5 || $nota3 < 5) {
         if ($nota1 < 5 && $nota2 < 5 && $nota3 < 5) {   
             $estado = "Deficiente";
         }
@@ -15,6 +18,6 @@ function clasificarNotas($nota1, $nota2, $nota3) {
         }
     }
 }
-clasificarNotas(9, 9, 9);
+$aprobado = clasificarNotas($nota1, $nota2, $nota3); 
 echo "El estado de la nota es $aprobado";
 ?>
