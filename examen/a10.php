4cf5e05ea1367ba912e2430ee6a0b6c62e67ced8
Kazus <120459752+GTAlex1@users.noreply.github.com>
2026-01-15 10:44:20 +0100

    a

--- examen/a10.php
+++ examen/a10.php
@@ -1,31 +1,34 @@
 <?php
-    function tablas($n, $tope) {
-        while ($n < $tope) {
-            echo "<table></table>";
-            for ($i=0; $i <=10 ; $i++) { 
+    function tablas($n, $tope, $c) {
+            echo "<table border='1' style='color:$c'>";
+            for ($i=0; $i <= $tope ; $i++) { 
                 $resultado = $n * $i;
                 echo "<tr><td>$n x $i = $resultado</td></tr>";
             }
-            $n++;
-        }
+            echo "</table>";
     }
 ?>
 <!DOCTYPE html>
 <html lang="es"> 
 <head>
-    <meta charset="UTF-8"
+    <meta charset="UTF-8">
     <meta name="viewport" content="widthdevice-width, initial-scale=1.0">
-    <title>Calculadora</title
+    <title>Calculadora</title>
 </head>
 <body>
     <?php 
     if(isset($_POST['enviar'])) {
-        tablas($_POST['n'], $_POST['tope']);
+        tablas($_POST['n'], $_POST['tope'], $_POST['color']);
     } else : ?>
     <form method="post">
         <p>Número: </p><input type="number" name="n" required>
         <p>Límite: </p><input type="number" name="tope" required>
-        <button type="submit" name"enviar">Enviar</button>
+        <select name="color" required>
+            <option value="red">Rojo</option>
+            <option value="green">Verde</option>
+            <option value="blue">Azul</option>
+        </select>
+        <button type="submit" name="enviar">Enviar</button>
     </form>
     <?php endif; ?>
 </body>
